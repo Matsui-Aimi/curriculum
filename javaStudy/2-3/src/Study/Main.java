6a4cc16263712ac1c0724684bad5c663650ca355
Matsui-Aimi <137156712+Matsui-Aimi@users.noreply.github.com>
2023-07-11 23:13:11 +0900

    2-3

---
+++ javaStudy/2-3/src/Study/Main.java
@@ -0,0 +1,12 @@
+package Study;
+
+public class Main {
+	public static void main(String[] args) {
+		Task task = new Task();
+
+		// ③ Taskクラスのインスタンスを生成し、「doTask()」メソッドを呼び出しなさい。
+		task.doTask();
+
+	}
+
+}
