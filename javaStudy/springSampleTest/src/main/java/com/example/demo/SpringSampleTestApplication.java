2139b7747809f14f3d95e8fa575615a938c2cfc5
Matsui-Aimi <137156712+Matsui-Aimi@users.noreply.github.com>
2023-10-10 22:43:03 +0900

    SpringSample

---
+++ javaStudy/springSampleTest/src/main/java/com/example/demo/SpringSampleTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSampleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSampleTestApplication.class, args);
+	}
+
+}
